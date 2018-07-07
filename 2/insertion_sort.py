703af8337a189c7e1f7ac13fb6a5acec1aab68eb
Low Jeng Lam <jenglamlow@Jeng.local>
2018-07-07 19:44:04 +0800

    Insertion Sort

---
+++ 2/insertion_sort.py
@@ -0,0 +1,16 @@
+def insertion_sort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        i = j - 1
+        while i >= 0 and A[i] > key:
+            A[i+1] = A[i]
+            i = i - 1
+        A[i+1] = key
+
+def main():
+    a = [9, 2, 1, 5, 6 , 10, 4]
+    insertion_sort(a)
+    print(a)
+
+if __name__ == '__main__':
+    main()
